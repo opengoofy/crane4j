8e31714105351a64bae35ccae6115690196df540
huangchengxing <841396397@qq.com>
2023-02-07 14:20:05 +0800

    test: add test cases

--- crane4j-core/src/test/java/cn/createsequence/crane4j/core/util/ReflectUtilsTest.java
+++ crane4j-core/src/test/java/cn/createsequence/crane4j/core/util/ReflectUtilsTest.java
@@ -1,10 +1,17 @@
 package cn.createsequence.crane4j.core.util;
 
+import cn.hutool.core.util.ReflectUtil;
 import lombok.SneakyThrows;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.stream.Stream;
 
@@ -14,6 +21,26 @@ import java.util.stream.Stream;
  * @author huangchengxing
  */
 public class ReflectUtilsTest {
+
+    @Test
+    public void putAnnotation() {
+        Method method = ReflectUtil.getMethod(ReflectUtilsTest.class, "putAnnotation");
+        Assert.assertNotNull(method);
+        Annotation annotation = method.getAnnotation(Annotation.class);
+        Assert.assertNull(annotation);
+        ReflectUtils.putAnnotation(Foo.class.getAnnotation(Annotation.class), method);
+        annotation = method.getAnnotation(Annotation.class);
+        Assert.assertNotNull(annotation);
+    }
+
+    @Test
+    public void setAttributeValue() {
+        Annotation annotation = Foo.class.getAnnotation(Annotation.class);
+        int expected = -1;
+        Assert.assertNotEquals(expected, annotation.value());
+        ReflectUtils.setAttributeValue(annotation, "value", expected);
+        Assert.assertEquals(expected, annotation.value());
+    }
 
     @Test
     public void getDeclaredFields() {
@@ -73,6 +100,14 @@ public class ReflectUtilsTest {
         );
     }
 
+    @Documented
+    @Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD, ElementType.TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    private @interface Annotation {
+        int value() default 0;
+    }
+
+    @Annotation
     private static class Foo {
 
         private Integer standard;
