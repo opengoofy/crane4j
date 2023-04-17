e64fa5c1325f0b8b288fb0541c8bcb53a0a0145e
huangchengxing <841396397@qq.com>
2023-04-17 12:21:31 +0800

    refactor(LazyAssembleOperation): obtain container through lazy loading (GitHub #50)

--- crane4j-core/src/main/java/cn/crane4j/core/parser/SimpleKeyTriggerOperation.java
+++ crane4j-core/src/main/java/cn/crane4j/core/parser/SimpleKeyTriggerOperation.java
@@ -11,6 +11,7 @@ import java.util.Set;
  *
  * @author huangchengxing
  * @see SimpleAssembleOperation
+ * @see LazyAssembleOperation
  * @see TypeFixedDisassembleOperation
  * @see TypeDynamitedDisassembleOperation
  */
--- crane4j-extension/crane4j-extension-mybatis-plus/src/main/java/cn/crane4j/extension/mybatis/plus/AssembleMpAnnotationResolver.java
+++ crane4j-extension/crane4j-extension-mybatis-plus/src/main/java/cn/crane4j/extension/mybatis/plus/AssembleMpAnnotationResolver.java
@@ -3,16 +3,31 @@ package cn.crane4j.extension.mybatis.plus;
 import cn.crane4j.annotation.AssembleMp;
 import cn.crane4j.core.container.Container;
 import cn.crane4j.core.executor.handler.AssembleOperationHandler;
-import cn.crane4j.core.parser.*;
+import cn.crane4j.core.parser.AbstractCacheableOperationAnnotationResolver;
+import cn.crane4j.core.parser.AssembleOperation;
+import cn.crane4j.core.parser.KeyTriggerOperation;
+import cn.crane4j.core.parser.LazyAssembleOperation;
+import cn.crane4j.core.parser.OperationAnnotationResolver;
+import cn.crane4j.core.parser.OperationParseContext;
+import cn.crane4j.core.parser.PropertyMapping;
+import cn.crane4j.core.parser.SimpleAssembleOperation;
 import cn.crane4j.core.support.AnnotationFinder;
 import cn.crane4j.core.support.Crane4jGlobalConfiguration;
 import cn.crane4j.core.support.Sorted;
 import cn.crane4j.core.util.ConfigurationUtil;
+import cn.crane4j.core.util.Lazy;
 import cn.crane4j.core.util.ReflectUtils;
 import cn.hutool.core.lang.Assert;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
 import java.lang.reflect.AnnotatedElement;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -27,10 +42,13 @@ import java.util.stream.Stream;
  * @see MpBaseMapperContainerRegister
  * @since 1.2.0
  */
+@Accessors(chain = true)
 public class AssembleMpAnnotationResolver extends AbstractCacheableOperationAnnotationResolver {
 
     private final MpBaseMapperContainerRegister mapperContainerRegister;
     private final Crane4jGlobalConfiguration globalConfiguration;
+    @Setter
+    private boolean lazyLoadAssembleContainer = true;
 
     /**
      * Create a {@link AssembleMpAnnotationResolver} instance.
@@ -99,18 +117,33 @@ public class AssembleMpAnnotationResolver extends AbstractCacheableOperationAnno
             globalConfiguration, annotation.handlerName(), annotation.handler()
         );
         Assert.notNull(handler, AbstractCacheableOperationAnnotationResolver.throwException("assemble operation handler [{}]({}) not found", annotation.handlerName(), annotation.handler()));
-        // get container
-        Container<?> container = mapperContainerRegister.getContainer(
-            annotation.mapper(), annotation.where(), Arrays.asList(annotation.selects())
-        );
+
         // parse property mapping
         Set<PropertyMapping> propertyMappings = Stream.of(annotation.props())
             .map(ConfigurationUtil::createPropertyMapping)
             .collect(Collectors.toSet());
         propertyMappings.addAll(ConfigurationUtil.parsePropTemplateClasses(annotation.propTemplates(), annotationFinder));
-        SimpleAssembleOperation operation = new SimpleAssembleOperation(
-            annotation.key(), annotation.sort(), propertyMappings, container, handler
-        );
+
+        // create operation
+        AssembleOperation operation;
+        if (lazyLoadAssembleContainer) {
+            Supplier<Container<?>> containerSupplier = new Lazy<>(() -> mapperContainerRegister.getContainer(
+                annotation.mapper(), annotation.where(), Arrays.asList(annotation.selects())
+            ));
+            operation = new LazyAssembleOperation(
+                annotation.key(), annotation.sort(),
+                propertyMappings, containerSupplier, handler
+            );
+        } else {
+            Container<?> container = mapperContainerRegister.getContainer(
+                annotation.mapper(), annotation.where(), Arrays.asList(annotation.selects())
+            );
+            operation = new SimpleAssembleOperation(
+                annotation.key(), annotation.sort(),
+                propertyMappings, container, handler
+            );
+        }
+
         // add groups
         operation.getGroups().addAll(Arrays.asList(annotation.groups()));
         return operation;
