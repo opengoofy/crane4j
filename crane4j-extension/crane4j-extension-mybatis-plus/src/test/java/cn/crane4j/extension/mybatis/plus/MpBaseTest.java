eac83eaa72aa1b43ac8762098f9cbab34079a518
huangchengxing <841396397@qq.com>
2023-05-08 12:32:22 +0800

    chore: replace mysql to h2 for database of crane4j-extension-mybatis-plus module test cases (GitHub #72)

--- crane4j-extension/crane4j-extension-mybatis-plus/src/test/java/cn/crane4j/extension/mybatis/plus/MpBaseTest.java
+++ crane4j-extension/crane4j-extension-mybatis-plus/src/test/java/cn/crane4j/extension/mybatis/plus/MpBaseTest.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.core.config.GlobalConfig;
 import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.toolkit.GlobalConfigUtils;
+import lombok.Cleanup;
 import lombok.SneakyThrows;
 import org.apache.ibatis.logging.slf4j.Slf4jImpl;
 import org.apache.ibatis.mapping.Environment;
@@ -17,6 +18,8 @@ import org.junit.After;
 import org.junit.Before;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,6 +27,21 @@ import java.util.Map;
  * @author huangchengxing
  */
 public abstract class MpBaseTest {
+
+    private static final String schema = "DROP TABLE IF EXISTS foo;"
+        + "CREATE TABLE `foo` ("
+        + "`id` int(11) NOT NULL AUTO_INCREMENT,"
+        + "`name` varchar(255) DEFAULT '',"
+        + "`age` int(3) DEFAULT NULL,"
+        + "`sex` int(1) DEFAULT NULL,"
+        + "PRIMARY KEY (`id`)"
+        + ");";
+    private static final String data = "DELETE FROM foo;"
+        + "INSERT INTO `foo`(`id`, `name`, `age`, `sex`) VALUES"
+        + "(1, '小明', 18, 1),"
+        + "(2, '小红', 18, 0),"
+        + "(3, '小刚', 17, 1),"
+        + "(4, '小李', 19, 0);";
 
     protected FooMapper fooMapper;
     private SqlSession sqlSession;
@@ -40,14 +58,24 @@ public abstract class MpBaseTest {
         globalConfig.setSuperMapperClass(BaseMapper.class);
         GlobalConfigUtils.setGlobalConfig(configuration, globalConfig);
 
+        // init data source and environment
         Map<String, String> properties = new HashMap<>();
-        properties.put("url", "jdbc:mysql://gz-cynosdbmysql-grp-nl9mays3.sql.tencentcdb.com:22327/crane4j-example?characterEncoding=utf8&useUnicode=true&useSSL=false&serverTimezone=Asia/Shanghai&&allowMultiQueries=true");
+        properties.put("url", "jdbc:h2:mem:test");
         properties.put("username", "crane4j");
         properties.put("password", "crane4j-test");
-        properties.put("driverClassName", "com.mysql.cj.jdbc.Driver");
+        properties.put("driverClassName", "org.h2.Driver");
         DataSource dataSource = DruidDataSourceFactory.createDataSource(properties);
         configuration.setEnvironment(new Environment("test", new JdbcTransactionFactory(), dataSource));
 
+        // init schema and data
+        @Cleanup
+        Connection connection = dataSource.getConnection();
+        @Cleanup
+        Statement statement = connection.createStatement();
+        statement.execute(schema);
+        statement.execute(data);
+
+        // create mapper proxy
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
         sqlSession = sqlSessionFactory.openSession();
         fooMapper = sqlSession.getMapper(FooMapper.class);
