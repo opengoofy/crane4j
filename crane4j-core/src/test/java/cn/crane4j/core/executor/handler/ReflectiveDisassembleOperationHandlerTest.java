2b793b1ccbb278bd91a8dba882cf908923de8ecc
balasukesh <bbelide2@fa23-cs527-002.cs.illinois.edu>
2023-11-12 21:22:04 -0600

    Fix flaky test in ReflectiveDisassembleOperationHandlerTest

--- crane4j-core/src/test/java/cn/crane4j/core/executor/handler/ReflectiveDisassembleOperationHandlerTest.java
+++ crane4j-core/src/test/java/cn/crane4j/core/executor/handler/ReflectiveDisassembleOperationHandlerTest.java
@@ -14,9 +14,11 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -37,24 +39,26 @@ public class ReflectiveDisassembleOperationHandlerTest extends BaseExecutorTest 
     public void process() {
         BeanOperations beanOperations = parseOperations(Bean.class);
         Collection<DisassembleOperation> operations = beanOperations.getDisassembleOperations();
+        List<DisassembleOperation> sortedOperations = new ArrayList<>(operations);
+        sortedOperations.sort(Comparator.comparing(DisassembleOperation::getKey));
 
         Bean root = new Bean();
-        DisassembleOperation operationForBean = CollectionUtils.get(operations, 0);
+        DisassembleOperation operationForBean = CollectionUtils.get(sortedOperations, 0);
         Assert.assertEquals("bean", operationForBean.getKey());
         root.setBean(new Bean());
         checkDisassembledBeans(operationForBean, root, 1);
 
-        DisassembleOperation operationForBeanArray = CollectionUtils.get(operations, 1);
+        DisassembleOperation operationForBeanArray = CollectionUtils.get(sortedOperations, 1);
         Assert.assertEquals("beanArray", operationForBeanArray.getKey());
         root.setBeanArray(new Bean[]{new Bean(), new Bean(), new Bean()});
         checkDisassembledBeans(operationForBeanArray, root, 3);
 
-        DisassembleOperation operationForBeanList = CollectionUtils.get(operations, 2);
+        DisassembleOperation operationForBeanList = CollectionUtils.get(sortedOperations, 2);
         Assert.assertEquals("beanList", operationForBeanList.getKey());
         root.setBeanList(Arrays.asList(new Bean(), new Bean(), new Bean()));
         checkDisassembledBeans(operationForBeanList, root, 3);
 
-        DisassembleOperation operationForBeanMultiList = CollectionUtils.get(operations, 3);
+        DisassembleOperation operationForBeanMultiList = CollectionUtils.get(sortedOperations, 3);
         Assert.assertEquals("beanMultiList", operationForBeanMultiList.getKey());
         root.setBeanMultiList(Arrays.asList(
             Arrays.asList(new Bean[]{new Bean(), new Bean()}, new Bean[]{new Bean(), new Bean()}),
